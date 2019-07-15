cd604ff5c28a51682e226e628b7bdf5ac4d792ac
Brian Saville <saville@adobe.com>
2019-07-15 09:44:13 -0600

    Move github deploy key decryption into publish-docs script

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-set -e
-
-echo "Generating Docs"
-
-./gradlew :spring-security-oauth2-provider:docs
 
 git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 
+echo "Decrypting github deploy key to publish docs"
+openssl aes-256-cbc -K $encrypted_c12e6c8df1af_key -iv $encrypted_c12e6c8df1af_iv -in github_deploy_key.enc -out /tmp/deploy_rsa -d
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa
+
+echo "Generating Docs"
+./gradlew :spring-security-oauth2-provider:docs
 
 echo "Publishing Documentation"
-
 rm -rf gh-pages
 git clone git@github.com:${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
 cd gh-pages
--- travis-build.sh
+++ travis-build.sh
@@ -5,8 +5,11 @@ echo "TRAVIS_TAG          : $TRAVIS_TAG"
 echo "TRAVIS_BRANCH       : $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST : $TRAVIS_PULL_REQUEST"
 echo "Publishing archives for branch $TRAVIS_BRANCH"
-rm -rf build
 
+# Clean workspace
+rm -rf build docs target gh-pages spring-security-oauth2-provider/build || true
+
+# Stop gradle daemon (if it is running) since it doesn't work well on travis
 ./gradlew --stop
 #TODO Fix integration tests, maybe using the acceptanceTest task?
 #./gradlew clean check assemble
